092a856ddac03e051d4d2f031cc8ea64a3e2def7
zhexiao <zhexiao@163.com>
2019-10-08 14:14:48 +0800

    转换为json数据

--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 )
 
 func main() {
+	//解析word
 	w := word.Word{}
 	w.Parser("./test/question-fill.docx")
 
+	//解析试题数据
 	q := word.Question{}
 	q.Parser(&w)
 
+	//转为json数据
 	jsonBytes, err := json.Marshal(q)
 	if err != nil {
 		log.Fatalf("json转换失败: %s", err)
