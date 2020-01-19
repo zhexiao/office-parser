0876cf63b457a01ef6a12b4f68569258342ea424
zhexiao <zhexiao@163.com>
2020-01-19 10:32:56 +0800

    新增convert测试

--- excel/convert_test.go
+++ excel/convert_test.go
@@ -4,6 +4,20 @@ import (
 	"testing"
 )
 
+func TestConvertFromFile(t *testing.T) {
+	_, err := ConvertFromFile("", "book")
+	if err == nil {
+		t.Error("传入空文件，应该报错")
+	}
+}
+
+func TestConvertFromData(t *testing.T) {
+	_, err := ConvertFromData([]byte{}, "book")
+	if err == nil {
+		t.Error("传入空字节，应该报错")
+	}
+}
+
 func TestConvertBook(t *testing.T) {
 	data, err := ConvertFromFile(bookTestFile, "book")
 	if err != nil {
@@ -13,6 +27,13 @@ func TestConvertBook(t *testing.T) {
 	data1 := data.(*CT_Book)
 	if len(data1.BookIndexs) != 18 {
 		t.Error("解析出来的数据有误，未匹配上数量")
+	}
+}
+
+func TestConvertErrorType(t *testing.T) {
+	_, err := ConvertFromFile(cognitionMapTestFile, "cognition")
+	if err == nil {
+		t.Error("错误类型，应该报错")
 	}
 }
 
